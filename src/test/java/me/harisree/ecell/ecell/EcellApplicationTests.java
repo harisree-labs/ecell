c843fcafe48cef01d6ecf0905ca29415dc0a292e
Sreehari B S <sreeharibs95@gmail.com>
2018-01-28 19:17:42 +0530

    Initial Commit

---
+++ src/test/java/me/harisree/ecell/ecell/EcellApplicationTests.java
@@ -0,0 +1,16 @@
+package me.harisree.ecell.ecell;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EcellApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
